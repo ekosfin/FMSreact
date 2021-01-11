3707d8a7728b39bda3268201ae566ac02c6d1dd8
ekosfin <elias.samuli@gmail.com>
2021-01-11 19:41:23 +0200

    added auth server
    still work in progress

--- auth/auth.js
+++ auth/auth.js
@@ -1,22 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
+const jwt = require("jsonwebtoken");
 const dbConnect = require("./controllers/dbConnect");
 const cors = require("cors");
+
+app.use(express.json());
 
 Object.assign = require("object-assign");
 app.use(morgan("combined"));
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-// error handling
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send("Something bad happened!");
-});
 
 app.listen(dbConnect.port, dbConnect.ip);
 console.log("Running on http://" + dbConnect.ip + ":" + dbConnect.port);
