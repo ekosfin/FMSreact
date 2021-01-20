5f30be7042596e5cdc3d4cfde807745d05235ae7
Suskiiii <susannah.jarventausta@gmail.com>
2021-01-20 20:15:18 +0200

    Created skeleton toggle button in Task Settings

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,47 +1,50 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import "../styles.css";
-import { eng } from '../languages/en.js';
-import { fin } from '../languages/fi.js';
-
+import React, {useState, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+import '../styles.css';
+import {eng} from '../languages/en.js';
+import {fin} from '../languages/fi.js';
 
 export default function Home(props) {
-    const history = useHistory();
-    const [language, setComponentLanguage] = useState(()  => getLangFromProp());
-    const [tasksList, setTasksList] = useState(["first", "second", "third"]);
+  const history = useHistory();
+  const [language, setComponentLanguage] = useState(() => getLangFromProp());
+  const [tasksList, setTasksList] = useState(['first', 'second', 'third']);
 
-    function getLangFromProp(){
-        if(props.language==="eng"){
-            return eng;
-        }else if(props.language==="fin"){
-            return fin;
-        } 
+  function getLangFromProp() {
+    if (props.language === 'eng') {
+      return eng;
+    } else if (props.language === 'fin') {
+      return fin;
     }
+  }
 
-    function onNewTaskClick(){
-        history.push("/newTask");
-    }
+  function onNewTaskClick() {
+    history.push('/newTask');
+  }
 
-    function updateTasksState(e) {
-        setTasksList(e);
-    }
+  function updateTasksState(e) {
+    setTasksList(e);
+  }
 
-    return (
-        <div className="Home">
-            <p className="smallText">{"The username"}</p>
-            <p className="pageTitle">{language.todo}</p>
-            <div className="todoList">
-                {tasksList.map((item) => (
-                    <p key={item} className="task">
-                        {item}
-                    </p>
-                ))}
-            </div>
-            <div className="homeButtonWrapper">
-                <button className="normalButton" id="homeNewTaskButton" onClick={onNewTaskClick}>
-                    {language.addNewTask}
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Home">
+      <p className="smallText">{'The username'}</p>
+      <p className="pageTitle">{language.todo}</p>
+      <div className="todoList">
+        {tasksList.map((item) => (
+          <p key={item} className="task">
+            {item}
+          </p>
+        ))}
+      </div>
+      <div className="homeButtonWrapper">
+        <button
+          className="normalButton"
+          id="homeNewTaskButton"
+          onClick={onNewTaskClick}
+        >
+          {language.addNewTask}
+        </button>
+      </div>
+    </div>
+  );
 }
