e2cba58a66784fcb82a8fa69aae33c3c16f3cdbf
apezkin <ape.zkin@gmail.com>
2021-01-20 21:07:02 +0200

    Add user info to Home view

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,47 +1,55 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import "../styles.css";
-import { eng } from '../languages/en.js';
-import { fin } from '../languages/fi.js';
-
+import { eng } from "../languages/en.js";
+import { fin } from "../languages/fi.js";
+import jwt_decode from "jwt-decode";
 
 export default function Home(props) {
-    const history = useHistory();
-    const [language, setComponentLanguage] = useState(()  => getLangFromProp());
-    const [tasksList, setTasksList] = useState(["first", "second", "third"]);
+  const history = useHistory();
+  const [language, setComponentLanguage] = useState(() => getLangFromProp());
+  const [tasksList, setTasksList] = useState(["first", "second", "third"]);
+  const user = jwt_decode(localStorage.getItem("accessToken"));
+  console.log(user);
 
-    function getLangFromProp(){
-        if(props.language==="eng"){
-            return eng;
-        }else if(props.language==="fin"){
-            return fin;
-        } 
+  function getLangFromProp() {
+    if (props.language === "eng") {
+      return eng;
+    } else if (props.language === "fin") {
+      return fin;
     }
+  }
 
-    function onNewTaskClick(){
-        history.push("/newTask");
-    }
+  function onNewTaskClick() {
+    history.push("/newTask");
+  }
 
-    function updateTasksState(e) {
-        setTasksList(e);
-    }
+  function updateTasksState(e) {
+    setTasksList(e);
+  }
 
-    return (
-        <div className="Home">
-            <p className="smallText">{"The username"}</p>
-            <p className="pageTitle">{language.todo}</p>
-            <div className="todoList">
-                {tasksList.map((item) => (
-                    <p key={item} className="task">
-                        {item}
-                    </p>
-                ))}
-            </div>
-            <div className="homeButtonWrapper">
-                <button className="normalButton" id="homeNewTaskButton" onClick={onNewTaskClick}>
-                    {language.addNewTask}
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Home">
+      <p className="smallText">
+        {user.username.length !== 0 ? user.username : user.email}
+      </p>
+      <p className="pageTitle">{language.todo}</p>
+      <div className="todoList">
+        {tasksList.map((item) => (
+          <p key={item} className="task">
+            {item}
+          </p>
+        ))}
+      </div>
+      <div className="homeButtonWrapper">
+        <button
+          className="normalButton"
+          id="homeNewTaskButton"
+          onClick={onNewTaskClick}
+        >
+          {language.addNewTask}
+        </button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,122 +1,124 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import "../styles.css";
-import { eng } from '../languages/en.js';
-import { fin } from '../languages/fi.js';
-
+import { eng } from "../languages/en.js";
+import { fin } from "../languages/fi.js";
 
 export default function Login(props) {
-    const history = useHistory();
-    const [language, setComponentLanguage] = useState(()  => getLangFromProp());
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const history = useHistory();
+  const [language, setComponentLanguage] = useState(() => getLangFromProp());
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    function getLangFromProp(){
-        if(props.language==="eng"){
-            return eng;
-        }else if(props.language==="fin"){
-            return fin;
-        } 
+  function getLangFromProp() {
+    if (props.language === "eng") {
+      return eng;
+    } else if (props.language === "fin") {
+      return fin;
     }
+  }
 
-    function onEngClick(){
-        props.setLanguage("eng");
-        updateComponentLanguage(eng);
-    }
+  function onEngClick() {
+    props.setLanguage("eng");
+    updateComponentLanguage(eng);
+  }
 
-    function onFinClick(){
-        props.setLanguage("fin");
-        updateComponentLanguage(fin);
-    }
+  function onFinClick() {
+    props.setLanguage("fin");
+    updateComponentLanguage(fin);
+  }
 
-    async function onLoginClick(){
-        const bodyData = {
-            username: username,
-            password: password
-          }
-        await fetch(
-        "http://localhost:4000/login", {
-            method: "POST",
-            mode: "cors",
-            headers: {"Content-Type":"application/json"},
-            body: JSON.stringify(bodyData)
-        }
-        ).then(response => response.json())
-        .then(data => {
-            localStorage.setItem("accessToken", data.accessToken);
-            localStorage.setItem("refreshToken", data.refreshToken);
-        });
+  async function onLoginClick() {
+    const bodyData = {
+      username: username,
+      password: password,
+    };
+    await fetch("http://localhost:4000/login", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(bodyData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem("accessToken", data.accessToken);
+        localStorage.setItem("refreshToken", data.refreshToken);
+      });
 
-        history.push("/home");
-    }
-    
-    function onRegisterClick(){
-        history.push("/register");
-    }
+    history.push("/home");
+  }
 
-    function engSelected(){
-        if(language===eng){
-            return "textOnlyButtonSelect"
-        }else{
-            return "textOnlyButton"
-        }
-    }
+  function onRegisterClick() {
+    history.push("/register");
+  }
 
-    function finSelected(){
-        if(language===fin){
-            return "textOnlyButtonSelect"
-        }else{
-            return "textOnlyButton"
-        }
+  function engSelected() {
+    if (language === eng) {
+      return "textOnlyButtonSelect";
+    } else {
+      return "textOnlyButton";
     }
+  }
 
-    function updateComponentLanguage(e){
-        setComponentLanguage(e);
+  function finSelected() {
+    if (language === fin) {
+      return "textOnlyButtonSelect";
+    } else {
+      return "textOnlyButton";
     }
+  }
 
-    function updateUsernameState(e) {
-        setUsername(e.target.value);
-    }
-    
-    function updatePasswordState(e) {
-        setPassword(e.target.value);
-    }
+  function updateComponentLanguage(e) {
+    setComponentLanguage(e);
+  }
 
-    return (
-        <div className="Login">
-            <div className="languageDiv">
-                <button className={engSelected()} onClick={onEngClick}>
-                    {language.enLang}
-                </button>
-                <button className={finSelected()} onClick={onFinClick}>
-                    {language.fiLang}
-                </button>
-            </div>
-            <p className="titleText">{language.title}</p>
-            <div className="loginInfoDiv">
-                <p className="normalText">{language.usEma}</p>
-                <input
-                    type="text"
-                    className="infoInput"
-                    value={username}
-                    onChange={updateUsernameState}
-                />
-                <p className="normalText">{language.password}</p>
-                <input
-                    type="text"
-                    className="infoInput"
-                    value={password}
-                    onChange={updatePasswordState}
-                />
-            </div>
-            <div className="loginButtonDiv">
-                <button className="normalButton" onClick={onLoginClick}>
-                    {language.login}
-                </button>
-                <button className="normalButton" id = "loginRegisterButton" onClick={onRegisterClick}>
-                    {language.register}
-                </button>
-            </div>
-        </div>
-    );
+  function updateUsernameState(e) {
+    setUsername(e.target.value);
+  }
+
+  function updatePasswordState(e) {
+    setPassword(e.target.value);
+  }
+
+  return (
+    <div className="Login">
+      <div className="languageDiv">
+        <button className={engSelected()} onClick={onEngClick}>
+          {language.enLang}
+        </button>
+        <button className={finSelected()} onClick={onFinClick}>
+          {language.fiLang}
+        </button>
+      </div>
+      <p className="titleText">{language.title}</p>
+      <div className="loginInfoDiv">
+        <p className="normalText">{language.usEma}</p>
+        <input
+          type="text"
+          className="infoInput"
+          value={username}
+          onChange={updateUsernameState}
+        />
+        <p className="normalText">{language.password}</p>
+        <input
+          type="text"
+          className="infoInput"
+          value={password}
+          onChange={updatePasswordState}
+        />
+      </div>
+      <div className="loginButtonDiv">
+        <button className="normalButton" onClick={onLoginClick}>
+          {language.login}
+        </button>
+        <button
+          className="normalButton"
+          id="loginRegisterButton"
+          onClick={onRegisterClick}
+        >
+          {language.register}
+        </button>
+      </div>
+    </div>
+  );
 }
