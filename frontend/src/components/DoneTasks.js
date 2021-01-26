7c1b3af273004ba561a1d1670e8f194da34f0b9a
Suskiiii <susannah.jarventausta@gmail.com>
2021-01-26 20:35:53 +0200

    Delete sample tasks

--- frontend/src/components/DoneTasks.js
+++ frontend/src/components/DoneTasks.js
@@ -1,36 +1,35 @@
-import React, { useState} from "react";
-import { useHistory } from "react-router-dom";
-import "../styles.css";
-import { eng } from '../languages/en.js';
-import { fin } from '../languages/fi.js';
-
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import '../styles.css';
+import {eng} from '../languages/en.js';
+import {fin} from '../languages/fi.js';
 
 export default function DoneTasks(props) {
-    const [language, setComponentLanguage] = useState(()  => getLangFromProp());
-    const [tasksList, setTasksList] = useState(["first", "second", "third"]);
+  const [language, setComponentLanguage] = useState(() => getLangFromProp());
+  const [tasksList, setTasksList] = useState([]);
 
-    function getLangFromProp(){
-        if(props.language==="eng"){
-            return eng;
-        }else if(props.language==="fin"){
-            return fin;
-        } 
+  function getLangFromProp() {
+    if (props.language === 'eng') {
+      return eng;
+    } else if (props.language === 'fin') {
+      return fin;
     }
+  }
 
-    function updateTasksState(e) {
-        setTasksList(e);
-    }
+  function updateTasksState(e) {
+    setTasksList(e);
+  }
 
-    return (
-        <div className="DoneTasks">
-            <p className="pageTitle">{language.doneTasks}</p>
-            <div className="todoList" id="completeList">
-                {tasksList.map((item) => (
-                    <p key={item} className="task">
-                        {item}
-                    </p>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="DoneTasks">
+      <p className="pageTitle">{language.doneTasks}</p>
+      <div className="todoList" id="completeList">
+        {tasksList.map((item) => (
+          <p key={item} className="task">
+            {item}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
